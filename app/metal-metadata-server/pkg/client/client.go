3812720f7cb881346f64e563ab82b950f194861e
Spencer Smith <robertspencersmith@gmail.com>
2020-09-28 16:22:42 -0400

    refactor: break apart metadata server code
    This breaks apart the metadata server to be more modular
    
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- app/metal-metadata-server/pkg/client/client.go
+++ app/metal-metadata-server/pkg/client/client.go
@@ -5,12 +5,22 @@
 package client
 
 import (
-	"k8s.io/client-go/dynamic"
+	cabpt "github.com/talos-systems/cluster-api-bootstrap-provider-talos/api/v1alpha3"
+	cacpt "github.com/talos-systems/cluster-api-control-plane-provider-talos/api/v1alpha3"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	caps "github.com/talos-systems/sidero/app/cluster-api-provider-sidero/api/v1alpha3"
+	metal "github.com/talos-systems/sidero/app/metal-controller-manager/api/v1alpha1"
 )
 
-func NewClient(kubeconfig *string) (dynamic.Interface, error) {
+// NewClient is responsible for creating a controller-runtime k8s client for use by the metadata server.
+func NewClient(kubeconfig *string) (client.Client, error) {
+	// Build rest config based on whether we've got a kubeconfig
 	var (
 		config *rest.Config
 		err    error
@@ -28,7 +38,35 @@ func NewClient(kubeconfig *string) (dynamic.Interface, error) {
 		}
 	}
 
-	c, err := dynamic.NewForConfig(config)
+	// Register all the schemes!
+	scheme := runtime.NewScheme()
+
+	if err = clientgoscheme.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	if err = capi.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	if err = cacpt.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	if err = cabpt.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	if err = caps.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	if err = metal.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+
+	// Finally create client
+	c, err := client.New(config, client.Options{Scheme: scheme})
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -6,19 +6,18 @@ require (
 	github.com/evanphx/json-patch v4.5.0+incompatible
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-logr/logr v0.1.0
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.3.3
-	github.com/hashicorp/go-multierror v1.0.0
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/golang/protobuf v1.4.2
+	github.com/hashicorp/go-multierror v1.1.0
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
 	github.com/pin/tftp v2.1.1-0.20200117065540-2f79be2dba4e+incompatible
+	github.com/talos-systems/cluster-api-bootstrap-provider-talos v0.2.0-alpha.3
+	github.com/talos-systems/cluster-api-control-plane-provider-talos v0.1.0-alpha.4
 	github.com/talos-systems/go-blockdevice v0.0.0-20200909151535-b71540f6c398
 	github.com/talos-systems/go-procfs v0.0.0-20200219015357-57c7311fdd45
 	github.com/talos-systems/go-smbios v0.0.0-20200219201045-94b8c4e489ee
 	github.com/talos-systems/talos v0.5.0
 	github.com/vmware/goipmi v0.0.0-20181114221114-2333cd82d702
-	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/sys v0.0.0-20200831180312-196b9ba8737a
 	google.golang.org/grpc v1.29.1
 	k8s.io/api v0.18.5
