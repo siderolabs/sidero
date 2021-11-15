b0e3611d2383061533e10d3bc1e642c99e9f70f9
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2021-11-15 21:06:26 +0300

    docs: update help message for extra kernel args
    Fixes #502
    
    The value is simply parsed as Linux kernel command line arguments.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- app/sidero-controller-manager/main.go
+++ app/sidero-controller-manager/main.go
@@ -87,7 +87,7 @@ func main() {
 	flag.IntVar(&apiPort, "api-port", 8081, "The TCP port Sidero components can be reached at from the servers.")
 	flag.IntVar(&httpPort, "http-port", 8081, "The TCP port Sidero controller manager HTTP server is running.")
 	flag.StringVar(&metricsAddr, "metrics-bind-addr", ":8081", "The address the metric endpoint binds to.")
-	flag.StringVar(&extraAgentKernelArgs, "extra-agent-kernel-args", "", "A comma delimited list of key-value pairs to be added to the agent environment kernel parameters.")
+	flag.StringVar(&extraAgentKernelArgs, "extra-agent-kernel-args", "", "A list of Linux kernel command line arguments to add to the agent environment kernel parameters (e.g. 'console=tty1 console=ttyS1').")
 	flag.StringVar(&bootFromDiskMethod, "boot-from-disk-method", string(ipxe.BootIPXEExit), "Default method to use to boot server from disk if it hits iPXE endpoint after install.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", true, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.BoolVar(&autoAcceptServers, "auto-accept-servers", false, "Add servers as 'accepted' when they register with Sidero API.")
