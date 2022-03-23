9438ca8e8aaafff3270bac85c5982918f3333278
Spencer Smith <spencer.smith@talos-systems.com>
2022-03-23 16:24:14 -0400

    chore: fix gpg check and sfyra build
    This PR fixes the gpg check as well as adds the clusterctl workaround to
    make sfyra build properly.
    
    Signed-off-by: Spencer Smith <spencer.smith@talos-systems.com>

--- hack/scripts/generate-clusterctl-config.sh
+++ hack/scripts/generate-clusterctl-config.sh
@@ -6,6 +6,12 @@ mkdir -p "$(dirname "${CLUSTERCTL_CONFIG}")"
 
 cat > "${CLUSTERCTL_CONFIG}" <<EOF
 providers:
+  - name: "talos"
+    url: "https://github.com/siderolabs/cluster-api-bootstrap-provider-talos/releases/latest/bootstrap-components.yaml"
+    type: "BootstrapProvider"
+  - name: "talos"
+    url: "https://github.com/siderolabs/cluster-api-control-plane-provider-talos/releases/latest/control-plane-components.yaml"
+    type: "ControlPlaneProvider"
   - name: "sidero"
     url: "file://${COMPONENTS_YAML}"
     type: "InfrastructureProvider"
